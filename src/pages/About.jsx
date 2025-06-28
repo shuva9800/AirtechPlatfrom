baccbcdcba8a1ac38d8f87b907c01f0260e5d943
shuva9800 <ffshuva@gmail.com>
2025-06-28 22:38:23 +0530

    About page style done

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -14,48 +14,50 @@ export default function About() {
   return (
     <div>
       {/* Section1 */}
-      <section>
-        <div className="w-11/12 mx-auto mt-5">
-          <header>
+      <section className="bg-richblack-700">
+        <div className="w-11/12 relative mx-auto mt-5 max-w-maxContent flex flex-col justify-between gap-10 text-center text-white">
+          <header className="mx-auto py-20 text-4xl font-semibold lg:w-[70%]">
             Driving Innovation in Online Education for a{" "}
             <HighlightText text={"Brighter Future"} />
           </header>
-          <p>
+          <p className="mx-auto mt-3 text-center text-base font-medium text-richblack-300 lg:w-[95%]">
             Studynotion is at the forefront of driving innovation in online
             education. We're passionate about creating a brighter future by
             offering cutting-edge courses, leveraging emerging technologies, and
             nurturing a vibrant learning community.
           </p>
-          <div className="flex gap-3">
-            <img src={BannerImage1} />
-            <img src={BannerImage2} />
-            <img src={BannerImage3} />
+          <div className="sm:h-[70px] lg:h-[150px]"></div>
+          <div className="absolute bottom-0 left-[-50%] grid w-[100%] translate-x-[-50%] translate-y-[30%] grid-cols-3 gap-3 lg:gap-5">
+            <img src={BannerImage1} alt="" />
+            <img src={BannerImage2} alt="" />
+            <img src={BannerImage3} alt="" />
           </div>
         </div>
       </section>
       {/* Section 2 */}
-      <section>
-        <div>
+      <section className="borer-b border-richblack-700">
+        <div className="mx-auto w-11/12 max-w-maxContent flex flex-col justify-between gap-10 text-richblack-500">
+          <div className="h-[100px] "></div>
           <Quote />
         </div>
       </section>
 
       {/* Section 3 */}
       <sectio >
-        <div className="w-11/12 mx-auto flex flex-col items-center justify-center">
+        <div className="w-11/12 mx-auto flex flex-col  justify-between gap-10 max-w-maxContent text-richblack-500">
           {/* Founding story div */}
-          <div className="flex gap-4">
+          <div className="flex gap-10 flex-col items-center lg:flex-row justify-between">
             {/* Foundng story left box */}
-            <div className="w-[45%]">
-              <h1 className="text-richblack-200">Our Founding Story </h1>
-              <p>
+            <div className="my-24 flex lg:w-[50%] flex-col gap-10">
+              <h1 className="bg-gradient-to-br from-[#833AB4] via-[#FD1D1D] to-[#FCB045] bg-clip-text text-4xl font-semibold text-transparent lg:w-[70%] ">Our Founding Story </h1>
+              <p className="text-base font-medium text-richblack-300 lg:w-[95%]">
                 Our e-learning platform was born out of a shared vision and
                 passion for transforming education. It all began with a group of
                 educators, technologists, and lifelong learners who recognized
                 the need for accessible, flexible, and high-quality learning
                 opportunities in a rapidly evolving digital world.
               </p>
-              <p>
+              <p className="text-base font-medium text-richblack-300 lg:w-[95%]">
                 As experienced educators ourselves, we witnessed firsthand the
                 limitations and challenges of traditional education systems. We
                 believed that education should not be confined to the walls of a
@@ -66,8 +68,8 @@ export default function About() {
               </p>
             </div>
             {/* Foundng story right box*/}
-            <div>
-              <img src={FundingStory} alt="Founding_story" />
+            <div >
+              <img src={FundingStory} alt="Founding_story" className="shadow-[0_0_20px_0] shadow-[#FC6767]" />
             </div>
           </div>
 
@@ -107,18 +109,20 @@ export default function About() {
 
 
       {/* Section 5 */}
-        <section className="w-11/12 mx-auto ">
+        <section className="w-11/12 mx-auto mt-20 flex max-w-maxContent flex-col justify-between gap-10 text-white ">
           <LearningGrid/>
         </section>
         {/* Section 4  Contact us Form*/}
-
-        <section className= "mb-[150px] " >
+        
+{/* ??? */}
+        <section className= "relative mx-auto w-11/12 mt-20 flex flex-col items-center justify-between gap-8 bg-richblack-900 max-w-maxContent text-white" >
           <ContactFormSection/>
         </section>
-        {/* Section 5 for Review section */}
-        <section>
-          <h1>Reviews from other learners</h1>
-        </section>
+
+        {/* Section 5 for Review section pending*/}
+        <div className= "relative mx-auto w-11/12 my-20 flex flex-col items-center justify-between gap-8 bg-richblack-900 max-w-maxContent text-white">
+          <h1 className="font-semibold text-4xl text-center mt-8">Reviews from other learners</h1>
+        </div>
 
         {/* footer section */}
         <Footer/>
