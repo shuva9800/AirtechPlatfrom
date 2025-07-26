ad0d116e65ec14977b19e1c2e47537af1a14d697
shuva9800 <ffshuva@gmail.com>
2025-07-26 13:07:18 +0530

    add fuction in Course_Card and CourseSlider

--- src/component/core/Catalog/CourseSlider.jsx
+++ src/component/core/Catalog/CourseSlider.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
-
+import {Swiper ,SwiperSlide} from 'swiper/react';
+import { Navigation, Pagination ,FreeMode} from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/free-mode';
+import Course_Card from './Course_Card';
 export default function CourseSlider({courses}) {
   return (
-    <div>CourseSlider</div>
+    <div>
+      {
+        courses?.length ?(
+          <Swiper>
+            {
+              courses.map((course,index)=>(
+                <SwiperSlide key={index}>
+                  <Course_Card course={course} Height={"h-[250px]"}/>
+                </SwiperSlide>
+              ))
+            }
+          </Swiper>
+        )
+        :(<p> No Course Found</p>)
+      }
+    </div>
   )
 }
--- src/component/core/Catalog/Course_Card.jsx
+++ src/component/core/Catalog/Course_Card.jsx
@@ -1,7 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import RatingStars from "../../common/RatingStars"
+import GetAvgRating from '../../../utils/avgRating';
 
-export default function Course_Card({course,key,Height}) {
+export default function Course_Card({course,Height}) {
+
+const [avgReviewCount , setAvgReviewCount] = useState(0);
+useEffect(()=>{
+  const count = GetAvgRating(course.ratingAndReview);
+  setAvgReviewCount(count)
+},[course])
+
+
+
   return (
-    <div>Course_Card</div>
+    <div>
+      <Link to={`/courses/${course._id}`}>
+        <div>
+          <div>
+            <img src={course.thumbnil} alt='courseThumbnil' className={`${Height} rounded-xl object-cover w-full`}/>
+          </div>
+          <div>
+            <p>{course?.courseName}</p>
+            <p>{course?.instractor?.firstName} {" "} {course?.instractor?.lastName} </p>
+            <div className='flex gap-x-3'>
+              <span>{avgReviewCount || 0}</span>
+              <RatingStars Review_Count={avgReviewCount}/>
+              <span>{course?.ratingAndReview?.length} Rating</span>
+            </div>
+          </div>
+          <p>{course?.price}</p>
+        </div>
+      </Link>
+    </div>
   )
 }
